0167bf82bed804f7760d7a8406c6281a2f370b72
m-abirhasan23 <abirhasanjoy07@gmail.com>
2023-04-03 00:54:59 +0600

    nav & footer

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const navData=[
+    {
+        path:"/",
+        name:"Home"
+    },
+    {
+        path:"/about",
+        name:"About"
+    },
+    {
+        path:"/work",
+        name:"Work"
+    },
+    {
+        path:"/journal",
+        name:"Journal"
+    },
+    {
+        path:"/contact",
+        name:"contact me"
+    },
+]
