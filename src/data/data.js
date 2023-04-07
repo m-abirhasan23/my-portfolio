04091d918d319208d4ad5d7ffdef75a7b16b65ca
m-abirhasan23 <abirhasanjoy07@gmail.com>
2023-04-07 19:51:57 +0600

    service section

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,74 @@
-export const navData=[
+export const navData = [
     {
-        path:"/",
-        name:"Home"
+        path: "/",
+        name: "Home"
     },
     {
-        path:"/about",
-        name:"About"
+        path: "/about",
+        name: "About"
     },
     {
-        path:"/work",
-        name:"Work"
+        path: "/work",
+        name: "Work"
     },
     {
-        path:"/journal",
-        name:"Journal"
+        path: "/journal",
+        name: "Journal"
     },
     {
-        path:"/contact",
-        name:"contact me"
+        path: "/contact",
+        name: "contact me"
+    },
+]
+
+
+export const serviceData = [
+    {
+        name: "html",
+        level: "intermediate"
+    },
+    {
+        name: "css",
+        level: "intermediate"
+    },
+    {
+        name: "tailwindcss",
+        level: "intermediate"
+    },
+    {
+        name: "sass/scss",
+        level: "intermediate"
+    },
+    {
+        name: "javascript",
+        level: "intermediate"
+    },
+    {
+        name: "react js",
+        level: "intermediate"
+    },
+    {
+        name: "next js",
+        level: "medium"
+    },
+    {
+        name: "astro js",
+        level: "medium"
+    },
+    {
+        name: "figma",
+        level: "basic"
+    },
+    {
+        name: "notion",
+        level: "basic"
+    },
+    {
+        name: "framer",
+        level: "basic"
+    },
+    {
+        name: "webflow",
+        level: "basic"
     },
 ]
